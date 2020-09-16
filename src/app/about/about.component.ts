ad508701eff16ecb974b526cee1a2faeef8672ad
mayaprebish <prebish.m@husky.neu.edu>
2020-09-16 13:30:11 -0400

    skills chart

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -6,23 +6,36 @@ import { Component, HostListener, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
-  @ViewChild('about-circle') aboutCircle: ElementRef;
-
-  rotation: any;
-  rotationCss = {
-    '-webkit-transform': 'rotate(0deg)',
-    '-moz-transform': 'rotate(0deg)',
-    'transform': 'rotate(0deg)',
-  }
+  @ViewChild('skillsChart') skillsChart: ElementRef
+  inView: boolean = false;
 
   @HostListener('window:scroll', ['$event'])
   handleScroll(){
-    const windowScroll = window.pageYOffset;
-    this.rotation = 140 + windowScroll / 3;
-    this.rotationCss = {
-      '-webkit-transform': 'rotate(' + this.rotation + 'deg)',
-      '-moz-transform': 'rotate(' + this.rotation + 'deg)',
-      'transform': 'rotate(' + this.rotation + 'deg)'
-    };
+    const scrollPosition = window.pageYOffset + window.innerHeight;
+    
+    if (this.skillsChart.nativeElement.offsetTop <= scrollPosition) {
+      this.inView = true;
+  } else {
+    this.inView = false;
+  }
   };
+
+  // yloc: any;
+  // pTitleCss = {
+  //   '-webkit-transform': 'translateY(0)',
+  //   '-moz-transform': 'translateY(0)',
+  //   'transform': 'translateY(0)',
+  // }
+
+  // @HostListener('window:scroll', ['$event'])
+  // handleScroll(){
+  //   const windowScroll = window.pageYOffset;
+  //   this.yloc = windowScroll / 3;
+  //   // this.pTitleCss = {
+  //   //   '-webkit-transform': 'translateY(' + this.yloc +'px)',
+  //   //   '-moz-transform': 'translateY(' + this.yloc + 'px)',
+  //   //   'transform': 'translateY(' + this.yloc + 'px)'
+  //   // };
+    
+  // };
 }

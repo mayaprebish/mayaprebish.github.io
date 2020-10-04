20c86d536b431f8b5f96c01990287ab280a963b0
mayaprebish <prebish.m@husky.neu.edu>
2020-10-04 19:22:55 -0400

    fix text

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -6,12 +6,4 @@ import { Component, HostListener, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent {
-  @ViewChild('experience-sample') headerText: ElementRef;
-
-  xloc: any;
-  sampleCss = {
-    '-webkit-transform': 'translateX(0)',
-    '-moz-transform': 'translateX(0)',
-    'transform': 'translateX(0)',
-  }
 }
